491312864efd8a301e6e23763f076ea322752022
Aditya Dandotia <webpro.aditya@gmail.com>
2026-03-25 18:00:46 +0530

    fix: font error

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -8,8 +8,6 @@
 
     <!-- Google Font: Source Sans Pro -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
-    <!-- Font Awesome -->
-    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
     <!-- icheck bootstrap -->
     <link rel="stylesheet" href="{{ asset('css/icheck-bootstrap.min.css') }}">
     <!-- Theme style -->
